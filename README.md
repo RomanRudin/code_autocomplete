5fba3b84ff909dcbc7b80b8af574aebf4c39d76b
Roman <rudin.roman@bk.ru>
2026-05-15 20:47:37 +0200

    Two better MLC models were added

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # code_autocomplete
+
+
+## Fututre plans
+
+- [] Restruucture project to be more modular and respect DRY
+- [] L&T: Evaluation
+- [] L: codet5p-770m-py instead of codet5p-220m-py
+- [] L: Salesforce/codegen-350M-mono (reuse for L)
+- [] Add third model (FIM-L): bigcode/santacoder (fill-in-the-middle)
+- [] L: bigcode/starcoder2-3b
+- [] T: Add more weight to punctuation tokens
+- [] L&T: try different lr, ctx, and etc
+- [] L&T: write cli or smth to try using it in coding
--- experiments/modules/HandTesting.py
+++ experiments/modules/HandTesting.py
@@ -1,10 +1,10 @@
 import torch
 from typing import Optional
 
-def hand_test_repl(token_model: Optional[torch.nn.Module], line_model: Optional[torch.nn.Module], tokenizer, hf_tok, device: torch.device):
-    print("  Python Autocomplete — Interactive Test")
-    print("  Commands: :line <prefix>  | :temp <float>  | :k <int> ")
-    print("            :token <prefix> | :quit")
+def hand_test_repl(token_model: Optional[torch.nn.Module], line_model: Optional[torch.nn.Module], tokenizer, hf_tok, device: torch.device, supports_multiline: bool = False):
+    greetings_text = "  Python Autocomplete — Interactive Test \n"
+    greetings_text += "  Commands:" + " :token <prefix> |" if token_model is not None else "" + " :line <prefix>  |" if line_model is not None else "" + " :multiline      |" if supports_multiline and line_model is not None else ""
+    greetings_text += "            :temp <float>   | :k <int>        | :quit           |"
 
     temperature = 0.3
     top_k       = 10
@@ -53,3 +53,25 @@ def hand_test_repl(token_model: Optional[torch.nn.Module], line_model: Optional[
                 )
             completion = hf_tok.decode(out[0], skip_special_tokens=True)
             print(f"  ← line  completion: {prefix}\033[33m{completion}\033[0m\n")
+        elif raw.startswith(":multiline") and line_model is not None:
+            if not supports_multiline:
+                print("This model does not support multiline mode")
+                continue
+            print(' MultiLine Context Model usage. Enter multi-line prefix, blank line to submit.')
+            buf = []
+            while True:
+                line = input(".. ")
+                if not line: break
+                buf.append(line)
+            prefix = "\n".join(buf)
+            inp = hf_tok(prefix, return_tensors="pt").to(device)
+            with torch.no_grad():
+                out = line_model.generate(
+                    **inp,
+                    max_new_tokens=64,
+                    temperature=temperature,
+                    do_sample=(temperature > 0.1),
+                    top_k=top_k,
+                )
+            completion = hf_tok.decode(out[0], skip_special_tokens=True)
+            print(f"  ← line  completion: \n\033[33m{prefix}\033[32m{completion}\033[0m\n")
